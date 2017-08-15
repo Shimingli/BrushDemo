382a00bb9e6bf7b2055d2fc01cab3ca46d5bc55d
Shimingli <qazwsx123>
2017-08-15 15:21:30 +0800

    提交更新的代码的代码

--- app/src/main/java/com/brush/opengldemo/compute/PhysicsCompute.java
+++ app/src/main/java/com/brush/opengldemo/compute/PhysicsCompute.java
@@ -1,9 +1,10 @@
 package com.brush.opengldemo.compute;
 
 import android.content.Context;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+
 
 import com.brush.opengldemo.ScriptC_physics;
 import com.brush.opengldemo.database.BristleParameters;
--- app/src/main/java/com/brush/opengldemo/vector/Vector3.java
+++ app/src/main/java/com/brush/opengldemo/vector/Vector3.java
@@ -1,7 +1,7 @@
 package com.brush.opengldemo.vector;
 
 
-import android.renderscript.Float3;
+import android.support.v8.renderscript.Float3;
 
 /** Defines a vector in 3D space */
 public class Vector3 {
